89d8354fcffcbd280840094b0de9d869425704f5
Kampouse <jpmartel98@gmail.com>
2020-01-07 20:20:40 +0000

    before i mess up

--- main.lua
+++ main.lua
@@ -2,31 +2,61 @@ local x = 2
 local y = 2
 local i = 1
 local x = 1
-local map = {1,4,1,1,1,4}
+local b = 4
+local h = 0
+BLOCK = 1
+PYRA = 2
+
+local map = {BLOCK,PYRA,1,1,1,4,1,1,2,2,2,2,2,2,2,2,2,2,2}
+local minima = {}
 local a = 0
-local e = -900    
-    function love.load()
-     for i = 1,6,1 
+local f = 0 
+local e = 1
+local same = {}
+
+    function love.load() -- howm many plaform you want or background
+     for i = 1,12,1 
         do
-            map[i] = love.graphics.newImage("tile"..map[i]..".png")
+           table.insert(same,love.graphics.newImage("tile"..map[i]..".png"))
+           table.insert(minima,love.graphics.newImage("tile"..map[2]..".png"))
+        for i,5,1
+            do      
+             
+    
         end
     end
+       
 print(x,y)
 function love.update(dt)--
-   e = e 
+   function love.keypressed(key)
+        if key == "left"then
+       e =  e + 20
+        end
+        if key == "right"then
+         e = e - 20
+        end
     end 
+end    
     function love.draw()
-        
-        for i = 1,6,1
+        for i = 1,8,1
         do
-            x = e + i * 600 + 200  
-            love.graphics.draw(map[i],x,y)
-        end 
+            x = e + i * 600 + 200 
+             love.graphics.draw(minima[i],e - 160 * i , 140) 
+             --love.graphics.draw(same[i],e - 500* i ,y)
+                 
+          
+            print(i)
+            print(f)
+        
+       
+       
+        
     end
+        
+       
+       
+       
 
-
-
-
-
+end
 
 
